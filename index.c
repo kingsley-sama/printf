d0ade77c5de07695bea837643a562e9a1a1b1b51
Gift <aghaulor.gift@gmail.com>
2023-11-11 08:53:07 -0800

    index.c

--- index.c
+++ index.c
@@ -1,12 +1,17 @@
 #include "main.h"
 #include <stdarg.h>
 
-
+/**
+ * _printf - function that prints output according to a format.
+ * @format: an array of characters and specifier
+ * Return:the number of characters printed
+ */
 
 int _printf(const char *format, ...)
 {
 	int count, i;
 	char c, s;
+	char *str;
 	va_list args;
 
 	count = 0;
@@ -23,6 +28,11 @@ int _printf(const char *format, ...)
 				_putchar(c);
 				i += 1;
 				break;
+			case 's':
+                                str = va_arg(args, char *);
+                                _print_str(str);
+				i += 1;
+                                break;
 			case '%':
 				_putchar('%');
 				break;
